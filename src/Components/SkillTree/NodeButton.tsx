5ebbffbf548803139d5ce6f50630077faf3927e1
Santiago Jiménez <santiago.abihatar.jimenez@correounivalle.edu.co>
2025-09-28 15:34:40 -0500

    Fixing deploy errors

--- src/Components/SkillTree/NodeButton.tsx
+++ src/Components/SkillTree/NodeButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 };
 
 export default function NodeButton({
-                                       x, y, image, name, isCompleted, isUnlocked, onClick, onEnter, onLeave,
+                                       x, y, image, name, isCompleted, onClick, onEnter, onLeave,
                                    }: Props) {
     return (
         <button
